81a3860140fa55076c8e334322db9574b85202da
Troland646 <troland646@gmail.com>
2024-05-28 04:03:46 -0700

    message

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS, THIS PAGE INDICATES SUCCESSS. ROLAND EMMANUEL TERRY VUG/CSC/21/5256'));
+// app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS, THIS PAGE INDICATES SUCCESSS. ROLAND EMMANUEL TERRY VUG/CSC/21/5256'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
